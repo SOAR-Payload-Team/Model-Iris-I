6bdd71886a796d74cf69f4480aa24c907fe157b3
Kail Olson <kail.olson@ucalgary.ca>
2023-02-04 14:48:23 -0700

    Second commit

---
+++ IRIS-1-Software/IRIS_1_Datalogging_Software/IRIS_1_Datalogging_Software.ino
@@ -0,0 +1,15 @@
+/**
+ * IRIS I PAYLOAD - DATALOGGING SOFTWARE
+ * 
+ * Microcontroller used: ESP32-WROVER
+ * 
+ * @author Kail Olson, Vardaan Malhotra
+ */
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
