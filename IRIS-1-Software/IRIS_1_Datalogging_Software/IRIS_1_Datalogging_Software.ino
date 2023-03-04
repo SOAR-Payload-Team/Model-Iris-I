4e205659e0b02f5c2f78973e83517209e4c11c3b
Aiden Ballard <aiden.ballard@ucalgary.ca>
2023-03-04 16:28:48 -0700

    Vardaan working on making SD card work???

--- IRIS-1-Software/IRIS_1_Datalogging_Software/IRIS_1_Datalogging_Software.ino
+++ IRIS-1-Software/IRIS_1_Datalogging_Software/IRIS_1_Datalogging_Software.ino
@@ -7,18 +7,17 @@
  */
 
 // including Arduino libraries
-#include "MS5611.h"
-#include <Adafruit_LSM6DSO32.h>
+#include "MS5611.h" //used for barometer
+#include <Adafruit_LSM6DSO32.h> //used for IMU
+#include "FS.h"
+#include "SD_MMC.h"
 
 // defining LED pins
 #define LED_1_PIN 25
 #define LED_2_PIN 26
 
-// defining IMU SPI pins
-#define LSM_CS 9
-#define LSM_SCK 13
-#define LSM_MISO 8
-#define LSM_MOSI 16
+// defining SPI pins
+
 
 // setting up the state machine enumerator
 enum State_Machine {
@@ -33,13 +32,14 @@ enum State_Machine {
 };
 
 // offsets generated from CALIBRATION
+/*
 int accel_calib_factor_x;
 int accel_calib_factor_y;
 int accel_calib_factor_z;
 int gyro_calib_factor_x;
 int gyro_calib_factor_y;
 int gyro_calib_factor_z;
-
+*/
 // setting up barometer library
 MS5611 MS5611(0x77);
 
@@ -48,6 +48,161 @@ Adafruit_LSM6DSO32 dso32;
 
 // Declaring the state variable
 State_Machine state = CALIBRATION;
+
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
 
 /**
  * @brief
@@ -85,7 +240,7 @@ void loop() {
             digitalWrite(LED_2_PIN, LOW);
 
             // initializing IMU
-            while(!dso32.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)){
+            while(!dso32.begin_I2C()){
                 Serial.println("LSM6DS032 not found.");
                 digitalWrite(LED_1_PIN, HIGH);
                 delay(500);
@@ -115,14 +270,52 @@ void loop() {
             dso32.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
             Serial.print("Gyro data rate set to: 12.5 Hz");
 
+  uint8_t cardType = SD_MMC.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+    Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+    listDir(SD_MMC, "/", 0);
+    createDir(SD_MMC, "/mydir");
+    listDir(SD_MMC, "/", 0);
+    removeDir(SD_MMC, "/mydir");
+    listDir(SD_MMC, "/", 2);
+    writeFile(SD_MMC, "/hello.txt", "Hello ");
+    appendFile(SD_MMC, "/hello.txt", "World!\n");
+    readFile(SD_MMC, "/hello.txt");
+    deleteFile(SD_MMC, "/foo.txt");
+    renameFile(SD_MMC, "/hello.txt", "/foo.txt");
+    readFile(SD_MMC, "/foo.txt");
+    testFileIO(SD_MMC, "/test.txt");
+    Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
+
+
             // move to CALIBRATION state for now
-            state = CALIBRATION;
+            state = LAUNCH;
             break;
         case READ_MEMORY:
             break;
         case ERASE_MEMORY:
             break;
         case CALIBRATION:
+            /*
             int calib_data = 50;
             int i;
             int accel_x[calib_data];
@@ -192,6 +385,7 @@ void loop() {
 
             // move to LAUNCH state for now
             state = LAUNCH;
+            */
             break;
         case PRELAUNCH:
             break;
@@ -207,38 +401,42 @@ void loop() {
             Serial.print("\n");
             delay(100);
 
-             // Get a new normalized sensor event
+            // Get a new normalized sensor event for the IMU
             sensors_event_t accel;
             sensors_event_t gyro;
             sensors_event_t temp;
             dso32.getEvent(&accel, &gyro, &temp);
 
+            /* Display the sensor results (temperature is measured in deg. C) */
             Serial.print("\t\tTemperature ");
             Serial.print(temp.temperature);
             Serial.println(" deg C");
 
-          //TODO: PUT THE CALIBRATION VALUES IN THE APPROPRIATE REGISTERS ON THE LSM6DSO32 INSTEAD OF ADDING TO THE VALUES AS DONE IN THE CODE BELOW???
-
-            /* Display the results (acceleration is measured in m/s^2) */
+            /* Display the sensor results (acceleration is measured in m/s^2) */
             Serial.print("\t\tAccel X: ");
-            Serial.print(accel.acceleration.x + accel_calib_factor_x);
+            //Serial.print(accel.acceleration.x + accel_calib_factor_x);
+            Serial.print(accel.acceleration.x);
             Serial.print(" \tY: ");
-            Serial.print(accel.acceleration.y + accel_calib_factor_y);
+            //Serial.print(accel.acceleration.y + accel_calib_factor_y);
+            Serial.print(accel.acceleration.y);
             Serial.print(" \tZ: ");
-            Serial.print(accel.acceleration.z + accel_calib_factor_z);
+            //Serial.print(accel.acceleration.z + accel_calib_factor_z);
+            Serial.print(accel.acceleration.z);
             Serial.println(" m/s^2 ");
 
-            /* Display the results (rotation is measured in rad/s) */
+            /* Display the sensor results (rotation is measured in rad/s) */
             Serial.print("\t\tGyro X: ");
-            Serial.print(gyro.gyro.x + gyro_calib_factor_x);
+            //Serial.print(gyro.gyro.x + gyro_calib_factor_x);
+            Serial.print(gyro.gyro.x);
             Serial.print(" \tY: ");
-            Serial.print(gyro.gyro.y + gyro_calib_factor_y);
+            //Serial.print(gyro.gyro.y + gyro_calib_factor_y);
+            Serial.print(gyro.gyro.y);
             Serial.print(" \tZ: ");
-            Serial.print(gyro.gyro.z + gyro_calib_factor_z);
+            //Serial.print(gyro.gyro.z + gyro_calib_factor_z);
+            Serial.print(gyro.gyro.z);
             Serial.println(" radians/s ");
             Serial.println();
             delay(100);
-            
             break;
         case KALMAN_FILTER:
             break;
