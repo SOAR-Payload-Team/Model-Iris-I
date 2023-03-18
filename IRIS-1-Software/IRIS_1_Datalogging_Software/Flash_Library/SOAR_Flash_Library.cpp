efc7472bc36cdac1f0c91b19f33d0fc17a3946e0
Vardaan Malhotra <vardaan.malh@gmail.com>
2023-03-18 18:30:32 -0300

    Adding more functionality

--- IRIS-1-Software/IRIS_1_Datalogging_Software/Flash_Library/SOAR_Flash_Library.cpp
+++ IRIS-1-Software/IRIS_1_Datalogging_Software/Flash_Library/SOAR_Flash_Library.cpp
@@ -20,8 +20,11 @@ initialize_flash()
 write_to_flash()
 read_from_flash()
 erase_flash()
-
 */
+
+
+
+
 
 /**
 * @brief Will check that the flash chip is returning the correct ID, it will also enable the flash chip's write mode.
@@ -41,53 +44,35 @@ uint8_t initialize_flash(int CS_pin, SPIClass SPI_peripheral, int chip_type) {
     // send a command to return the chip's IDs
     SPI_peripheral.transfer(0x9F); // 9Fh is the get JEDEC ID command
     
-    uint8_t manufacturer_ID_recieved = SPI_peripheral.transfer(0x00); // first byte returned is the manufacturer ID
+    uint8_t manufacturer_ID = SPI_peripheral.transfer(0x00); // first byte returned is the manufacturer ID
     uint16_t device_ID = SPI_peripheral.transfer16(0x0000); // next two bytes are the device ID
     
-    // end the transaction
-    SPI_peripheral.endTransaction();
-    digitalWrite(CS_pin, HIGH);
-    
-    delay(1);
-    
-    // enable the write mode <--- THIS NEEDS TO BE CALLED EVERYTIME, IT IS RESET TO 0 AFTER THE WRITE INSTRUCTION IS CMOPLETED, SO A SEPERATE FUNCTION MAY BE REQUIRED
-    digitalWrite(CS_pin, LOW);
-    SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0));
-    SPI_peripheral.transfer(0x06); // op-code for write enable
-    digitalWrite(CS_pin, HIGH);     //de-assert the chip select line
-    SPI_peripheral.endTransaction();
-    
+    digitalWrite(CS_pin, HIGH); //de-assert the CS line
+    SPI_peripheral.endTransaction(); // end the transaction
+
     //Setting default non-IDs
     uint16_t chip_ID = 0x1111;
-    uint8_t manufacturer_ID = 0x11
 
     // manufacturer ID for W25Q1 is 0xEF, chip ID for W25Q128 is 0x4018, chip ID for W25Q512 is 0x4020
-    // manufacturer ID for AT25SF is 0x1F, chip ID for AT25SF321B is 0x8701
-    if (chip_type == 128) {
-        manufacturer_ID == 0xEF;
-        chip_ID = 0x4018;
-    }
-    else if (chip_type == 512) {
-        manufacturer_ID == 0xEF;
-        chip_ID = 0x4020;
-    }
-    else if (chip_type == 321) {
-        manufacturer_ID == 0x1F;
-        chip_ID = 0x8701;
-    }
+    if (chip_type == 128) chip_ID = 0x4018;
+    else if (chip_type == 512) chip_ID = 0x4020;
     
-    return (manufacturer_ID_recieved == manufacturer_ID || device_ID == chip_ID) ? 1 : 0;
+    return (manufacturer_ID == 0xEF || device_ID == chip_ID) ? 1 : 0;
 }
 
+
+
+
+
 /**
-* @brief Writes the number of given bytes in a buffer of 8-bit integers
+* @brief Writes the number of given bytes in a buffer of 8-bit integers.
 */
-uint8_t write_to_flash(int CS_pin, SPIClass SPI_peripheral, uint32_t* address, uint8_t* buffer, int number_of_bytes) {
+uint8_t write_to_flash(int CS_pin, SPIClass SPI_peripheral, uint32_t current_address, uint8_t* buffer, int number_of_bytes) {
     
     uint8_t status; // returns 0 on failure and 1 on success                    
 
     if(number_of_bytes > 256){
-        status = 1;
+        status = 0;
         return status; //if the number of bytes to write to memory exceeds a page (256 bytes), the write_to_flash function should not execute
     } 
     
@@ -96,63 +81,59 @@ uint8_t write_to_flash(int CS_pin, SPIClass SPI_peripheral, uint32_t* address, u
     // checks if address is 4 bytes or 3 bytes
     if (0xFF000000 & address) {
         page_program_opcode = 0x12; // for 4 byte address (W25Q512 only)
+        int address_counter = 4; //address is 32 bits
     } else {
         page_program_opcode = 0x02; // for 3 byte address
+        int address_counter = 3; //address is 24 bits
     }
     
+    write_enable(CS_pin, SPI_peripheral);   //enabling the write bit within Status Register 1
+
     // begin the transaction
     digitalWrite(CS_pin, LOW);
     SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0)); // rising edge of clock, MSB first
     
-    //add code here
-
-    uint32_t end_current_page = (0xFFFFFF00 & *address) + 0xFF; //isolate the most significant bits and add 1 page size (0xFF = 256 bytes)
-    uint32_t current_page_space = end_current_page - *address;
-    uint32_t write_address;
-
+    uint32_t end_current_page = (0xFFFFFF00 & current_address) + 0xFF; //isolate the most significant bits and add 1 page size (0xFF = 256 bytes)
+    uint32_t current_page_space = end_current_page - current_address;
+        
     if (current_page_space >= number_of_bytes){ //if the amount of data to write fits within the current page, write normally
-        write_address = *address;
 
         SPI_peripheral.transfer(page_program_opcode);
-        SPI_peripheral.transfer(write_address);
-        for (int i = 0; i < number_of_bytes; i++) {
-            SPI_peripheral.transfer(buffer[i]);
-        }
-
+        for(int i = 0; i < address_counter; i++) SPI_peripheral.transfer(current_address >> 8*i); //transferring address
+        SPI_peripheral.transfer(buffer[0], number_of_bytes); //transfering data
+ 
         digitalWrite(CS_pin, HIGH); //de-assert the CS pin for the command to execute
-
-        *address = *address + number_of_bytes;
 
         //poll the BUSY bit to ensure that the data has been written before de-asserting the chip select line
         while (BUSY_bit_check(CS_pin, SPI_peripheral)){};
+
+        current_address = address_manager(current_address, number_of_bytes); //updating the address
+
         status = 1;
     }
     else{ //if the amount of data to write does not fit within the current page, split the data into seperate packages
-        write_address = *address;
+            
         SPI_peripheral.transfer(page_program_opcode);
-        SPI_peripheral.transfer(write_address);
-        for (int i = 0; i < current_page_space; i++) {
-            SPI_peripheral.transfer(buffer[i]);
-        }
+        for(int i = 0; i < address_counter; i++) SPI_peripheral.transfer(current_address >> 8*i); //transferring address
+        SPI_peripheral.transfer(buffer[0], current_page_space); //transfering data
 
-        *address = *address + current_page_space;
-        write_address = *address;
-        uint32_t remaining_bytes = number_of_bytes - current_page_space;
-
-        SPI_peripheral.transfer(page_program_opcode);
-        SPI_peripheral.transfer(write_address);
-        for (int i = 0; i < remaining_bytes; i++) {
-            SPI_peripheral.transfer(buffer[i]);
-        }
-        
         digitalWrite(CS_pin, HIGH); //de-assert the CS pin for the command to execute
 
-         *address = *address + remaining_bytes;
+        current_address = address_manager(current_address, current_page_space); //updating the address
+        uint32_t remaining_bytes = number_of_bytes - current_page_space;
+
+        SPI_peripheral.transfer(page_program_opcode); //transfering operation code
+        for(int i = 0; i < address_counter; i++) SPI_peripheral.transfer(current_address >> 8*i); //transferring address
+        SPI_peripheral.transfer(buffer[current_page_space], remaining_bytes); //transfering data
+
+        digitalWrite(CS_pin, HIGH); //de-assert the CS pin for the command to successfully execute
 
         //poll the BUSY bit to ensure that the data has been written before de-asserting the chip select line
         while (BUSY_bit_check(CS_pin, SPI_peripheral)){};
+        
+        current_address = address_manager(current_address, remaining_bytes); //updating the address
+       
         status = 1;
-
     }  
 
     // end the transaction
@@ -161,10 +142,69 @@ uint8_t write_to_flash(int CS_pin, SPIClass SPI_peripheral, uint32_t* address, u
     return status;
 }
 
+
+
+
+/**
+* @brief Reads from flash memory.
+*/
+uint8_t read_from_flash(int CS_pin, SPIClass SPI_peripheral, uint32_t address_start, uint32_t address_end) {
+    // distinguishing between chip types
+   
+    uint8_t read_data_op_code = 0;
+    
+    // checks if address is 4 bytes or 3 bytes
+    if (0xFF000000 & address) {
+        read_data_op_code = 0x13; // for 4 byte address (W25Q512 only)
+        int address_counter = 4; //address is 32 bits
+    } else {
+        read_data_op_code = 0x03; // for 3 byte address
+        int address_counter = 3; //address is 24 bits
+    }
+    // begin the transaction
+    digitalWrite(CS_pin LOW);
+    SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0)); // rising edge of clock, MSB first
+
+     // send a command to read data starting at address_start
+    SPI_peripheral.transfer(read_data_op_code);
+    SPI_peripheral.transfer(start_address);
+}
+
+
+
+
+
+/**
+* @brief Erases all of the data in the flash chip
+*/
+uint8_t chip_erase(int CS_pin, SPIClass SPI_peripheral){
+    
+    write_enable(CS_pin, SPI_peripheral);   //enabling the write bit within Status Register 1
+
+    // begin the transaction
+    digitalWrite(CS_pin, LOW);
+    SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0)); // rising edge of clock, MSB first
+
+    // send a command to erase ALL data on flash chip
+    SPI_peripheral.transfer(0xC7);
+    digitalWrite(CS_pin, HIGH); //de-assert the CS line
+
+    //Wait for chip erase to take its typiacl 40 seconds. After that, poll the BUSY bit to ensure that the data has been written before de-asserting the chip select line
+    delaty(40000);
+    while (BUSY_bit_check(CS_pin, SPI_peripheral)){};
+
+    SPI_peripheral.endTransaction(); // end the transaction
+}
+
+
+
+
+
+/**
+* @brief Checks the status of the BUSY bit lcoated in Status Register 1.
+*/
 uint8_t BUSY_bit_check(int CS_pin, SPIClass SPI_peripheral){
-    
-    //SPI_peripheral.beginTransaction is not executed as BUSY_bit_check should only be executed within another function where the SPI settings have already been set
-    
+    //SPI_peripheral.beginTransaction(...) and SPI_peripheral.endTransaction() is not executed as BUSY_bit_check should only be executed within another function where the SPI settings have already been set
     digitalWrite(CS_pin, LOW);
     SPI_peripheral.transfer(0x05);
     uint8_t status_register = SPI.transaction(0); //obtains the values within status register 1
@@ -174,16 +214,35 @@ uint8_t BUSY_bit_check(int CS_pin, SPIClass SPI_peripheral){
     return BUSY_bit;
 }
 
-uint8_t erase_flash(){
-//ABOVE CODE CAN BE REUSED FOR THIS
+
+
+
+
+/**
+* @brief Write enable to allow Page Program, Chip Erase and Write Status Register commansd to be executed.
+*/
+void write_enable(int CS_pin, SPICLass SPI_peripheral){
+
+    digitalWrite(CS_pin, LOW);
+    SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0)); // rising edge of clock, MSB first
+    SPI_peripheral.transfer(0x06); // op-code for write enable
+    digitalWrite(CS_pin, HIGH); //de-assert the chip select line
+    SPI_peripheral.endTransaction(); // end the transaction
+}
+
+
+
+
+/**
+* @brief Function used to monitor the current address.
+*/
+uint32_t address_manager(uint32_t current_address, int bytes_written){
+    
+    if(bytes_written == 0) return address; //function call occured to find the value of the current address
+    else{
+        address = current_address + bytes_written; //function call occured to increment the current address to the next available address to which data can be written
+        return address;
+    }
 }
 //TODO: use assert statements to stop the program from running a function if an error occurs. maybe try to error handle though!
-//TODO: write erase_flash
-//TODO: move write_enable into a function 
-/*    digitalWrite(CS_pin, LOW);
-    SPI_peripheral.beginTransaction(SPISettings(26000000, MSBFIRST, SPI_MODE0));
-    SPI_peripheral.transfer(0x06); // op-code for write enable
-    digitalWrite(CS_pin, HIGH);     //de-assert the chip select line
-    SPI_peripheral.endTransaction();
-*/
-//TODO: write raed_memory
+//TODO: write read_memory
